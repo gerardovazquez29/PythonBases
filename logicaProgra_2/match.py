23650f15b10122f254adc036d3e33c2fb2b4b46c
Gerardo Vazquez <neowhatup@gmail.com>
2025-02-20 17:51:48 -0600

    m

--- logicaProgra_2/match.py
+++ logicaProgra_2/match.py
@@ -1,4 +1,5 @@
-
+# Description: Ejemplo de uso de match en Python 3.10
+# match
 serie = "N-02"
 
 match serie:
@@ -11,14 +12,31 @@ match serie:
     case _:
         print("Serie no encontrada")
 
-cliente = {'nombre': 'Juan', 'edad': 25, 'ciudad': 'Lima'}
 
-pelicula = {'titulo': 'El padrino', 'año': 1972, 'director': 'Francis Ford Coppola'}
+# match con patrones
+
+cliente = {'nombre': 'Juan', 
+           'edad': 25, 
+           'ciudad': 'Lima'}
+
+pelicula = {'titulo': 'El padrino', 
+            'año': 1972, 
+            'director': 'Francis Ford Coppola'}
 
 elementos = [cliente, pelicula, 'libro']
 
 for elemento in elementos:
     match elemento:
-        case {'nombre': nombre, 'edad': edad, 'ciudad': ciudad}:
+        case {'nombre': nombre, 
+              'edad': edad, 
+              'ciudad': ciudad}:
             print("es un cliente")
             print(f"Nombre: {nombre}, Edad: {edad}, Ciudad: {ciudad}")
+        case {'titulo': titulo, 
+              'año': año, 
+              'director': director}:
+            print("es una pelicula")
+            print(f"Título: {titulo}, Año: {año}, Director: {director}")
+        case _:
+            print("No se puede identificar el elemento")
+               
